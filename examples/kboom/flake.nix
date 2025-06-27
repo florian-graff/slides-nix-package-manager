faf3380e79ab0deef4b68609b7b14933b5babd45
Florian Graff <florian.graff@klosebrothers.de>
2025-06-27 07:59:41 +0200

    slide 46

--- examples/kboom/flake.nix
+++ examples/kboom/flake.nix
@@ -11,5 +11,15 @@
     }:
     flake-utils.lib.eachDefaultSystem (system: let
         pkgs = import nixpkgs { inherit system; };
-    in {});
+    in {
+        devShells.default = pkgs.mkShell {
+                nativeBuildInputs = with pkgs; [
+                  gcc
+                  gdb
+                  gmp
+                  gnumake
+                  rubyPackages.rspec
+                ];
+        };
+    });
 }
